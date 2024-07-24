3e19fb1918e9e51d7ce0557ba5c49813b96c1aa0
Wakhi-Ken <w.dlamini@alustudent.com>
2024-07-24 12:28:12 +0200

    app

--- creating_student_rec.py
+++ creating_student_rec.py
@@ -10,45 +10,28 @@ class Student:
         self.credit_score = credit_score
         self.gpa = gpa
 
-def student_records():
-    if os.path.exists('records.json') and os.path.getsize('records.json') > 0:
-        with open('records.json', 'r') as file:
+def load_existing_data():
+    if os.path.exists('./student_rec.json') and os.path.getsize('./student_rec.json') > 0:
+        with open('./student_rec.json', 'r') as file:
             return json.load(file)
     else:
-        default_students = [
-            {
-                "name": "Prince",
-                "last_name": "Doe",
-                "email": "prince.doe@gmail.com",
-                "course_name": "Introduction to Python",
-                "credit_score": "70",
-                "gpa": "3.4"
-            },
-            {
-                "name": "Luke",
-                "last_name": "Smith",
-                "email": "luke.smith@gmail.com",
-                "course_name": "Introduction to SQL",
-                "credit_score": "80",
-                "gpa": "3.9"
-            }
-        ]
-        return default_students
+        return []
 
 def save_student(student_data):
-    with open('records.json', 'w') as file:
+    with open('./student_rec.json', 'w') as file:
         json.dump(student_data, file, indent=4)
 
-def creating_student():
+def creating_student_rec():
     print("Please enter student information:")
     name = input("Name: ")
     last_name = input("Last Name: ")
     email = input("Email: ")
-    course_name = input("Course Name: ")
-    credit_score = input("Credit Score: ")
-    gpa = input("GPA: ")
+    Course_name = input("Course Name: ")
+    Credit_score = input("Credit Score: ")
+    Gpa  = input("Gpa: ")
 
-    new_student = Student(name, last_name, email, course_name, credit_score, gpa)
+
+    new_student = Student(name, last_name, email, Course_name, Credit_score, Gpa)
 
     print("\nNew student added successfully:")
     print("Name:", new_student.name, new_student.last_name)
@@ -57,7 +40,9 @@ def creating_student():
     print("Credit Score:", new_student.credit_score)
     print("GPA:", new_student.gpa)
 
-    existing_data = student_records()
+    existing_data = load_existing_data()
+
+    print(f"Existing data: {existing_data}")
 
     existing_data.append({
         "name": new_student.name,
